50fcd7b75eb7f7c1a0adadc81d7a713f792e5b68
Clément Gendry <clement.gendry@hetic.net>
2015-02-12 17:07:14 +0100

    encrypt database - table - Film

--- ruby_film/app/controllers/links_controller.rb
+++ ruby_film/app/controllers/links_controller.rb
@@ -13,7 +13,9 @@ class LinksController < ApplicationController
             redirect_to :back
         else
             @link = current_user.links.build(:film_id => params[:film_id], :status => params[:status])
+            @film = current_user.films.build(:name => params[:name], :description => params[:description], :url => params[:url])
             @link.save
+            @film.save
             p @link.errors
             redirect_to :back
         end
